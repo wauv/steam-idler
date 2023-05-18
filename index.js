fa799a0ace64a0306c49952caa3e5e6a95e335fb
Pashka <64590900+wauv@users.noreply.github.com>
2023-05-18 22:06:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = 'process.env.username';
 var password = 'process.env.password';
 var shared_secret = 'process.env.shared';
 
-var games = [730, 252490, 381210];  // Enter here AppIDs of the needed games
+var games = [730, 252490, 381210];  // Enter here App IDs of the games
 var status = 1;  // 1 - online, 7 - invisible
 
 
